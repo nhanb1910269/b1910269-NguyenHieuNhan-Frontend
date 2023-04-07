e3e4a2cefc86a7306566281113c8d7302c08d6d9
nhanb1910269 <nhanb1910269@student.ctu.edu.vn>
2023-04-08 05:33:37 +0700

    Khoi tao du an

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+
+}
+</script>
+<template>
+  <h1>I am not in danger, Skyler. I am the danger. A guy opens his door and gets shot and you think that of me? NO. I am
+    the one who knocks</h1>
+</template>
+<style>
+.page {
+  max-width: 400px;
+  margin: auto;
+}
+</style>
